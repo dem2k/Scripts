6afa70097fe3de99d77caa8773d3f5230fbb4342
Dimitri Kleyn <dimitri.kleyn@fv.nrw.de>
2018-10-19 18:34:55 +0200

    Initial Commit.

---
+++ LibKingDownloader.ps1
@@ -0,0 +1,105 @@
+Param([Parameter(Mandatory)]$Url, [Parameter(Mandatory)]$Pages)
+
+Set-StrictMode -Version Latest
+
+function Main() {
+    Get-Fb2Header
+    for ($i = 1; $i -le $Pages; $i++) {
+        $progressPreference = 'silentlyContinue'
+        "<section id=""seite_$i"">"
+        Fetch-Page ( $Url -replace "/(\d+?)-", "/`${1}-$i-" )
+        # "<p>-- $i --</p>"
+        "</section>"
+        $progressPreference = 'Continue'
+        Write-Progress -Activity "Downloading Pages..." -Status "Page $i" -PercentComplete ($i/$Pages*100)
+    }
+    Get-Fb2Footer
+}
+
+function Fetch-DocumentInfo($Url) {
+    $content = (Invoke-WebRequest $Url | select Content) -split "`n"
+    $content | where {$_ -match '<meta property="og:title" content="(.*?)" />'} | Out-Null
+    if ($Matches) {
+        $Matches[1] -split " - "
+    }
+    else {
+        "Unknown Author", "Unknown Title"
+    }
+
+    $content | where {$_ -match '<a itemprop="genre".*?>(.*?)</a>'} | Out-Null
+    if ($Matches) {
+        $Matches[1].tolower()
+    }
+    else {
+        "unrecognised"
+    }
+
+}
+
+function Fetch-Page ($Url) {
+    $lines = (Invoke-WebRequest $Url | select Content) -split "`n"
+    Filter-PageContent $lines | foreach {Filter-Html $_}
+}
+
+function Filter-PageContent($RawContent) {
+    $startIdx = 0
+    $endIdx = 0
+    $divnav = $false
+    for ($i = 0; $i -lt $RawContent.Count; $i++) {
+        $cursor = $RawContent[$i]
+        if ($cursor.StartsWith('<div class="navigation">')) {
+            $divnav = $true
+        }
+        if ($divnav -and ($startIdx -eq 0) -and ($cursor -eq "</div>")) {
+            $startIdx = $i + 1
+        }
+        if (($startIdx -gt 0) -and ($cursor.StartsWith( "<script"))) {
+            $endIdx = $i - 1
+            break
+        }
+    }
+    $RawContent[$startIdx..$endIdx]
+}
+
+function Filter-Html($line) {
+    $line -replace "<div.*?>", "<title>" -replace "</div>", "</title>" -replace "&nbsp;"," " -replace "<br>",""
+}
+
+function Get-Fb2Header { 
+    @"
+<?xml version="1.0" encoding="UTF-8"?>
+<FictionBook xmlns="http://www.gribuser.ru/xml/fictionbook/2.0" xmlns:xlink="http://www.w3.org/1999/xlink">
+<description>
+    <description>
+        <title-info>
+            <genre>$Genre</genre>
+            <author>$Author</author>
+            <book-title>$Title</book-title>
+            <lang>ru</lang>
+        </title-info>
+		<document-info>
+            <author>LibKing.ru</author>
+            <program-used>LibKingDownloader.ps1 by dem2k</program-used>
+            <date>$(Get-Date)</date>
+            <src-url>$Url</src-url>
+			<id>$(New-Guid)</id>
+			<version>3.1415901184082031</version>
+		</document-info>
+    </description>
+<body>
+"@
+}
+    
+function Get-Fb2Footer {
+    @"
+</body>
+</FictionBook>
+"@
+}
+
+#$progressPreference = "silentlyContinue"
+$Author, $Title, $Genre = Fetch-DocumentInfo $Url
+$outputFile = "{0} - {1}.fb2" -f $Author, $Title
+Main | Out-File $outputFile -Encoding utf8
+
+#Pause
